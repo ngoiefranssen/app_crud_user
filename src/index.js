3f40e2ef26f690eac4de75a6f88ba168c1292d6d
Franssen <ngoiefranssen04@gmail.com>
2022-12-12 14:55:30 +0200

    Wip

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css'
 import reportWebVitals from './reportWebVitals';
 
 const rootElementRoute = ReactDOM.createRoot(document.getElementById('root'));
