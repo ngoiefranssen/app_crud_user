018913844156d861106e896163b8fecff0a7838e
Franssen <ngoiefranssen04@gmail.com>
2022-12-12 17:00:37 +0200

    Wip

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,35 +2,34 @@
 export const Home = () => {
     return (
         <div className="container">
-   <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-        </table>
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">city</th>
+                    <th scope="col">Name_company</th>
+                    <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Ngoie</td>
+                    <td>julia</td>
+                    <td>@mdo</td>
+                    <td>Beta</td>
+                    <td>cassandra</td>
+                    <td>
+                        <button className="btn btn-primary"></button>
+                        <button className="btn btn-primary"></button>
+                        <button className="btn btn-primary"></button>
+                    </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
      
     );
