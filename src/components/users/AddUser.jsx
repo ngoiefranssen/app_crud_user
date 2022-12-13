b274ac28b31c837154bbbf3e2c94bbb08ecf6b99
Franssen <ngoiefranssen04@gmail.com>
2022-12-13 12:07:19 +0200

    Wip

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -69,8 +69,8 @@ export const AddUser = () => {
 <section className="background-radial-gradient overflow-hidden">
   <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
     <div className="row gx-lg-5 align-items-center mb-5">
-      <div className="col-lg-6 mb-5 mb-lg-0" style="z-index: 10">
-        <h1 className="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+      <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex:"10px" }}>
+        <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color:"hsl(218, 81%, 95%)" }}>
           The best offer <br />
           <span style="color: hsl(218, 81%, 75%)">for your business</span>
         </h1>
