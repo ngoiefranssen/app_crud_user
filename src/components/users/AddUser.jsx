df75f1efc0600e2c9fe17b7e8fff63457476b98a
Franssen <ngoiefranssen04@gmail.com>
2022-12-13 14:35:36 +0200

    Wi

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -69,63 +69,60 @@ export const AddUser = () => {
 <section className="background-radial-gradient overflow-hidden">
   <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
     <div className="row gx-lg-5 align-items-center mb-5">
-      <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex:"10px" }}>
+      {/* <div className="col-lg-6 mb-5 mb-lg-0" style={{ zIndex:"10px" }}>
         <h1 className="my-5 display-5 fw-bold ls-tight" style={{ color:"hsl(218, 81%, 95%)" }}>
           The best offer <br />
-          <span style="color: hsl(218, 81%, 75%)">for your business</span>
+          <span style={{ color:"hsl(218, 81%, 75%)" }}>for your business</span>
         </h1>
-        <p className="mb-4 opacity-70" style="color: hsl(218, 81%, 85%)">
+        <p className="mb-4 opacity-70" style={{ color:"hsl(218, 81%, 85%)" }}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           Temporibus, expedita iusto veniam atque, magni tempora mollitia
           dolorum consequatur nulla, neque debitis eos reprehenderit quasi
           ab ipsum nisi dolorem modi. Quos?
         </p>
-      </div>
+      </div> */}
 
-      <div className="col-lg-6 mb-5 mb-lg-0 position-relative">
-        <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-        <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+      <div className="col-lg-12 mb-5 mb-lg-0 position-relative">
+        {/* <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div> */}
+        {/* <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div> */}
 
-        <div className="card bg-glass">
-          <div className="card-body px-4 py-5 px-md-5">
+        <div className="card">
+          <div className="card-body px-4 py-5">
             <form>
               <div className="row">
-                <div className="col-md-6 mb-4">
+                <div className="col-md-4 mb-4">
                   <div className="form-outline">
-                    <input type="text" id="form3Example1" className="form-control" />
-                    <label className="form-label" for="form3Example1">First name</label>
+                    <input type="text" className="form-control"
+                            placeholder='Enter your name...' />
                   </div>
                 </div>
-                <div className="col-md-6 mb-4">
+                <div className="col-md-4 mb-4">
                   <div className="form-outline">
-                    <input type="text" id="form3Example2" className="form-control" />
-                    <label className="form-label" for="form3Example2">Last name</label>
+                    <input type="text"  className="form-control"
+                            placeholder='Enter your username....'/>
+                  </div>
+                </div>
+                <div className="col-md-4 mb-4">
+                  <div className="form-outline">
+                    <input type="text" className="form-control"
+                           placeholder='Enter your adress email...' />
                   </div>
                 </div>
               </div>
 
               <div className="form-outline mb-4">
-                <input type="email" id="form3Example3" className="form-control" />
-                <label className="form-label" for="form3Example3">Email address</label>
+                <input type="email" className="form-control" />
               </div>
 
               <div className="form-outline mb-4">
-                <input type="password" id="form3Example4" className="form-control" />
-                <label className="form-label" for="form3Example4">Password</label>
+                <input type="password" className="form-control" />
               </div>
 
-              <div className="form-check d-flex justify-content-center mb-4">
-                <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33"/>
-                <label className="form-check-label" for="form2Example33">
-                  Subscribe to our newsletter
-                </label>
-              </div>
-
-              <button type="submit" className="btn btn-primary btn-block mb-4">
-                Sign up
+              <button type="submit" className="btn btn-outline-dark">
+                Register
               </button>
 
-              <div className="text-center">
+              {/* <div className="text-center">
                 <p>or sign up with:</p>
                 <button type="button" className="btn btn-link btn-floating mx-1">
                   <i className="fab fa-facebook-f"></i>
@@ -142,7 +139,7 @@ export const AddUser = () => {
                 <button type="button" className="btn btn-link btn-floating mx-1">
                   <i className="fab fa-github"></i>
                 </button>
-              </div>
+              </div> */}
             </form>
           </div>
         </div>
