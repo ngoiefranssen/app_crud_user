404c63e6a2a60995e08fed7dd279cf6e78642082
Franssen <ngoiefranssen04@gmail.com>
2022-12-13 11:38:31 +0200

    Wip

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -3,21 +3,83 @@
 export const AddUser = () => {
 
   return (
-    <div className="container justify-center">
-      <div className="row">
-        <div className="col-md-6">
-          <div className="form-control">
-          <input type="text" />
-          <input type="text" className="ms-3" />
-          </div>
-          
-        </div>
-        <div className="col-md-6">
-        <input type="text" />
-
-        </div>
-        <div className="col-md-6"></div>
+    <section className="text-center">
+      <div className="p-5 bg-image" style="
+            background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg');
+            height: 300px;
+            ">
       </div>
-    </div>
-  );
+
+      <div className="card mx-4 mx-md-5 shadow-5-strong" style="
+            margin-top: -100px;
+            background: hsla(0, 0%, 100%, 0.8);
+            backdrop-filter: blur(30px);
+            ">
+        <div className="card-body py-5 px-md-5">
+
+          <div className="row d-flex justify-content-center">
+            <div className="col-lg-8">
+              <h2 className="fw-bold mb-5">Sign up now</h2>
+              <form>
+                <div className="row">
+                  <div className="col-md-6 mb-4">
+                    <div className="form-outline">
+                      <input type="text"  className="form-control" />
+                      <label className="form-label" for="form3Example1">First name</label>
+                    </div>
+                  </div>
+                  <div className="col-md-6 mb-4">
+                    <div className="form-outline">
+                      <input type="text" id="form3Example2" className="form-control" />
+                      <label className="form-label" for="form3Example2">Last name</label>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input type="email" id="form3Example3" className="form-control" />
+                  <label className="form-label" for="form3Example3">Email address</label>
+                </div>
+
+                <div className="form-outline mb-4">
+                  <input type="password" id="form3Example4" className="form-control" />
+                  <label className="form-label" for="form3Example4">Password</label>
+                </div>
+
+                <div className="form-check d-flex justify-content-center mb-4">
+                  <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+                  <label className="form-check-label" for="form2Example33">
+                    Subscribe to our newsletter
+                  </label>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block mb-4">
+                  Sign up
+                </button>
+
+                <div className="text-center">
+                  <p>or sign up with:</p>
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-facebook-f"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-google"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-twitter"></i>
+                  </button>
+
+                  <button type="button" className="btn btn-link btn-floating mx-1">
+                    <i className="fab fa-github"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+ );
 };
