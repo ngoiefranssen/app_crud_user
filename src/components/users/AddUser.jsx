175c1082b138c2541c0c40fce196cc2b437c7478
Franssen <ngoiefranssen04@gmail.com>
2022-12-13 11:33:13 +0200

    Wip

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -1,13 +1,23 @@
 
 
-
 export const AddUser = () => {
 
-
-
   return (
-    <>
+    <div className="container justify-center">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="form-control">
+          <input type="text" />
+          <input type="text" className="ms-3" />
+          </div>
+          
+        </div>
+        <div className="col-md-6">
+        <input type="text" />
 
-    </>
+        </div>
+        <div className="col-md-6"></div>
+      </div>
+    </div>
   );
 };
