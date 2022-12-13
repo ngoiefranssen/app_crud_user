8315cd4aaca0b590b49bdccd2ffbae73cec57547
Franssen <ngoiefranssen04@gmail.com>
2022-12-13 14:09:02 +0200

    Wip

--- src/components/users/AddUser.jsx
+++ src/components/users/AddUser.jsx
@@ -105,19 +105,16 @@ export const AddUser = () => {
                 </div>
               </div>
 
-              <!-- Email input -->
               <div className="form-outline mb-4">
                 <input type="email" id="form3Example3" className="form-control" />
                 <label className="form-label" for="form3Example3">Email address</label>
               </div>
 
-              <!-- Password input -->
               <div className="form-outline mb-4">
                 <input type="password" id="form3Example4" className="form-control" />
                 <label className="form-label" for="form3Example4">Password</label>
               </div>
 
-              <!-- Checkbox -->
               <div className="form-check d-flex justify-content-center mb-4">
                 <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
                 <label className="form-check-label" for="form2Example33">
@@ -125,12 +122,10 @@ export const AddUser = () => {
                 </label>
               </div>
 
-              <!-- Submit button -->
               <button type="submit" className="btn btn-primary btn-block mb-4">
                 Sign up
               </button>
 
-              <!-- Register buttons -->
               <div className="text-center">
                 <p>or sign up with:</p>
                 <button type="button" className="btn btn-link btn-floating mx-1">
